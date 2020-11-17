04cc3f8384d7f429434b5eada48ae8dab95bc757
choyrulh <choirul.humam@students.amikom.ac.id>
2020-11-17 19:08:09 +0700

    Add project files.

---
+++ FreshFuit/Controller/BucketController.cs
@@ -0,0 +1,55 @@
+﻿using FreshFuit;
+using FreshFuit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshFruit.controller
+{
+    class BucketController
+    {
+        private Bucket bucket;
+        private BucketEventListener eventListener;
+        private MainWindow mainWindow;
+
+        public BucketController(Bucket bucket, BucketEventListener eventListener)
+        {
+            this.bucket = bucket;
+            this.eventListener = eventListener;
+        }
+
+        public void addFruit(Fruit fruit)
+        {
+            if (bucketIsOverload())
+            {
+                eventListener.onFailed("Ops, Keranjang penuh bro");
+            }
+            else
+            {
+                this.bucket.insert(fruit);
+                eventListener.onSucceed("Yey, Berhasil ditambahkan bro");
+            }
+        }
+        public bool bucketIsOverload()
+        {
+            return bucket.countItems() >= bucket.getCapacity();
+        }
+        public void removeFruit(Fruit fruit)
+        {
+            for (int itemPosition = 0; itemPosition < bucket.countItems(); itemPosition++)
+            {
+                if (bucket.findAll().ElementAt(itemPosition).getName() == fruit.getName())
+                {
+                    bucket.remove(itemPosition);
+                    eventListener.onSucceed("Yey, berhasil dihapus bro");
+                }
+            }
+        }
+        public List<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+    }
+}
